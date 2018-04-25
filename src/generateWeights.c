e6b122451bef25a596710c42d8c4e0c1b4d255e7
ybhuiyan94 <bhuiyan.yasir@gmail.com>
2018-04-25 17:21:19 -0400

    generateWeights now prints out all calculated weights

--- src/generateWeights.c
+++ src/generateWeights.c
@@ -11,6 +11,7 @@
 
 void initArrays();
 void readFreqFile(FILE *in, int category);
+void calculateWeights();
 
 char trackingStrings[MAXWORDSTRACKED][MAXWORDSIZE];
 double weightVector[MAXWORDSTRACKED][CATEGORYCOUNT];
@@ -24,8 +25,10 @@ int main(int argc, char **argv) {
 
 	for(i = 1; i <= CATEGORYCOUNT; i++){
 		in = fopen(argv[i], "r");
-		readFreqFile(in, i);
+		readFreqFile(in, i-1);
 	}
+
+	calculateWeights();
 }
 
 // put strings in string array, frequencies in weightVector
@@ -47,9 +50,42 @@ void initArrays() {
 void readFreqFile(FILE *in, int category) {
 	char word[MAXWORDSIZE];
 	int freq;
+	int found = -1;
+	int i;
 
 	while(fscanf(in, "%s %d", &word, &freq) == 2)  {
-			printf("%s %d\n", &word, freq);
-      totalWords++;
+			for(i = 0; i < totalWords; i++) {
+				if(strcmp(word, trackingStrings[i]) == 0){
+					found = i;
+					break;
+				}
+			}
+
+			if(found == -1) {
+				strcpy(trackingStrings[totalWords], word);
+				weightVector[totalWords][category] = (double) freq;
+				totalWords++;
+			} else {
+				weightVector[found][category] = (double) freq;
+			}
   }
+}
+
+void calculateWeights() {
+	int i, y;
+	double sum;
+
+	for(i = 0; i < totalWords; i++) {
+		sum = 0;
+		for(y = 0; y < CATEGORYCOUNT; y++) {
+			sum += weightVector[i][y];
+		}
+		for(y = 0; y < CATEGORYCOUNT; y++) {
+			weightVector[i][y] = weightVector[i][y] / sum;
+			printf("%lf", weightVector[i][y]);
+		}
+		printf("\n");
+
+	}
+
 }

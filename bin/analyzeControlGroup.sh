4b8f24580a2f21802d0600934fb790afaad20935
ybhuiyan94 <bhuiyan.yasir@gmail.com>
2018-04-25 17:52:26 -0400

    Updated shell script
    Generates frequency files, then a weight file from them

--- bin/analyzeControlGroup.sh
+++ bin/analyzeControlGroup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file runs wordFrequency on the contents of each of the categories in controlgroup/
-# It will create an individual file with word totals for each category.
+# It will create a file "weights.txt" that contains the weight vectors used in analyzeText
 rm -f output.txt
 for file in ../controlGroup/moneyArticles/*
 do
@@ -19,3 +19,9 @@ do
 	./wordFrequency "$file"
 done
 mv output.txt sportsFrequency.txt
+
+./generateWeights moneyFrequency.txt politicsFrequency.txt sportsFrequency.txt
+
+rm moneyFrequency.txt politicsFrequency.txt sportsFrequency.txt
+
+
